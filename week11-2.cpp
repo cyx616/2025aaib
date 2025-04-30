49655fd28cdeb2d7671597dc2b954fa803fdd83a
cyx616 <13750333@me.mcu.edu.tw>
2025-04-30 11:49:54 +0800

    week11

---
+++ week11-2.cpp
@@ -0,0 +1,18 @@
+///week11-2.cpp
+
+#include <stdio.h>
+
+int addnum(int a,int b)
+{
+    printf("現在進入addnum()函式裡，a:%d b:%d\n",a,b);
+    int x;
+    x=a+b;
+    printf("兩樹相加後，得到數值%d將return回傳\n",x);
+    return x;
+}
+
+int main()
+{
+    int ans = addnum(10,30);///
+    printf("%d",ans);
+}
